9c66f127241a03dedc988bc6dd9f4327d11eeb90
jhgarry <junghg05@gmail.com>
2025-07-25 17:31:24 +0900

    added basic animation and edited README

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let i = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  textAlign(CENTER);
+  rectMode(CENTER);
+  fill(255);
 }
 
 function draw() {
   background(0);
+  text("Welcome to my Page!", width/2, height/2);
+  push();
+  fill(0);
+  translate(width/2, height/2);
+  rect(i, 0, 200, 25)
+  i++;
+  pop();
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
